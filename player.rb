e0187c03871145beb99a96f15f472228d1af7a9f
Saïd Redouane <you@example.com>
2021-03-29 22:45:36 +0000

    Create player class

---
+++ player.rb
@@ -0,0 +1,20 @@
+class Player
+
+  attr_accessor :name, :number_of_lives
+
+  def initialize(n)
+    self.name = n
+    self.number_of_lives = 3
+  end
+
+  def lose_life
+    self.number_of_lives -= 1
+  end 
+
+end
+
+player1 = Player.new("Rona")
+puts player1.name
+puts player1.number_of_lives
+player1.lose_life
+puts player1.number_of_lives
